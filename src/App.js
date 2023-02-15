e8f00f195ddb7da7b06b7c035609a050a9426a06
Spencer Creer <spencercreer@gmail.com>
2023-02-15 09:55:00 -0700

    added Canvas, Ground, and Sky

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
-import './App.css';
+import { Canvas } from '@react-three/fiber';
+import { Sky } from '@react-three/drei';
+import { Physics } from '@react-three/cannon';
+import { Ground } from './components/Ground';
 
 function App() {
   return (
-    <div className="App">App</div>
+    <>
+    <Canvas>
+      <Sky sunPosition={[100, 100, 20]} />
+      <ambientLight intensity={0.5} />
+      <Physics >
+        <Ground />
+      </Physics>
+    </Canvas>
+    </>
   );
 }
 
---
+++ src/components/Ground.js
@@ -0,0 +1,20 @@
+import { RepeatWrapping } from "three";
+import { usePlane } from "@react-three/cannon";
+import { groundTexture } from "../images/textures";
+
+export const Ground = () => {
+    const [ref] = usePlane(() => ({
+        rotation: [0,0,0], position: [0,0,0]
+    }))
+
+    groundTexture.wrapS = RepeatWrapping
+    groundTexture.wrapT = RepeatWrapping
+    groundTexture.repeat.set(100, 100)
+
+    return (
+        <mesh ref={ref}>
+            <planeBufferGeometry attach='geometry' args={[100, 100]} />
+            <meshStandardMaterial attach='material' map={groundTexture} />
+        </mesh>
+    )
+}
