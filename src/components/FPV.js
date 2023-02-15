1147087ac018544915ca81a3c17b43636b937de3
Spencer Creer <spencercreer@gmail.com>
2023-02-15 14:21:26 -0700

    First person view

---
+++ src/components/FPV.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { useThree } from "@react-three/fiber"
+import { PointerLockControls } from "@react-three/drei"
+
+export const FPV = () => {
+    const { camera, gl } = useThree()
+
+  return <PointerLockControls args={[camera, gl.domElement]} />
+}
